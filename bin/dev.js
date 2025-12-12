7b7c58ef62a6ded1f4c5873f74f03e5efd4cbfad
Johnny Robeson <johnny@localmomentum.net>
2025-12-12 05:55:21 -0500

    refactor: Drop everything except the driver. kysely 0.29 will ship one

--- bin/dev.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-// eslint-disable-next-line n/shebang
-import { execute } from '@oclif/core'
-
-await execute({ development: true, dir: import.meta.url })
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,3 @@ export { PGliteDialect } from './pglite-dialect.js'
 export { type PGliteDialectConfig } from './pglite-dialect-config.js'
 
 export { KyselyPGlite } from './kysely-pglite.js'
-
-export { KyselyLive } from './kysely-live.js'
-
-export { createMigrator } from './utils/create-migrator.js'
